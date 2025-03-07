be35056e702df2a31744a20df5ddc8689237265d
aisakov04 <aisakov@umass.edu>
2025-03-07 11:55:55 -0500

    implemented sine function

--- calculator.js
+++ calculator.js
@@ -29,6 +29,7 @@ function naturalLog(value) {
 // TODO: Implement sine function
 function sine(value) {
     // return the sine of the value
+    return Math.sin(value * Math.PI/180);
 }
 
 // TODO: Implement cosine function
